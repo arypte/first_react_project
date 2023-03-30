7de3b42f85af94cb5804e27bee83edcec3188722
arypte <wjs505@gmail.com>
2023-03-30 16:47:10 +0900

    my_portfolio
    기본 화면 구성

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -17,6 +17,69 @@ function App() {
           </ul>
         </div>
       </header>
+      <main>
+        {/* Introduce */}
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          자기소개
+        </div>
+
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center items-center gap-8 py-8">
+          {/* margin padding */}
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
+        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 연락처
+        </div>
+      </main>
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:p2-2">
+        designed by ~~
+      </footer>
     </div>
   );
 }
